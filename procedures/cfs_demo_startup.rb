9a78f2f09cc4b9ee5b41f1edac1ef9d39fc06636
Niall Mullane <niall.p.mullane@gmail.com>
2021-07-20 16:17:03 -0400

    Create amp cosmos demo startup script

---
+++ procedures/cfs_demo_startup.rb
@@ -0,0 +1,8 @@
+# Simple procedure to startup the cfs cosmos demo for the AMP testbed
+# Author: Niall Mullane
+def enable_telemetry_output
+	cmd("TO_LAB TO_LAB_ENABLE_OUTPUT")
+end
+
+
+enable_telemetry_output()
